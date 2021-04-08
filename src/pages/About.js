85a593ceb570d9e176d43c4292dbf991dcdb3dd1
Leandro Parrado <lparrado321@gmail.com>
2021-04-08 14:47:19 -0400

    edited description

--- src/pages/About.js
+++ src/pages/About.js
@@ -22,17 +22,14 @@ function About() {
                 <div className="col-md-4">
                   <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/File_000.jpeg" alt="Placeholder" className="img-thumbnail" />
                 </div>
-                <p className="card-text text-wrap"> Hey there! I'm Leandro. I was born in Kissimmee, Florida, spent a large
+                <p className="card-text text-wrap"> Hey there! I'm Leandro. I was born in Kissimmee, Florida and spent a large
                 chunk of my childhood in Las Vegas, Nevada, before moving back down to Florida and have been living here
                 ever since! In 2017, I graduated from Hialeah Gardens H.S. in Hialeah, FL. After completing some courses
                 in Graphic Design, I decided to change majors and tried a career in trades as an electrician. I did a lot
               of soul searching before my gut told me to finally go with web development.</p>
 
-                <p className="card-text text-wrap">Now enrolled as a student in the University of Central Florida's Coding
-                Bootcamp as of December 7, 2020,
-                I'm finally pursuing something that is fulfilling and challenges me to grow. I've learned to work with
-                HTML, CSS, Javascript, Bootstrap, Jquery, APIs, and much more. My goal is to one day become a full stack
-              developer and start my own company. Thanks for visiting my web site.</p>
+                <p className="card-text text-wrap">Now I'm finally pursuing something that is fulfilling and challenges me to grow. After graduating from the UCF Full-Stack Web Development bootcamp, I'm able to build beautiful full-stack applications and solve problems with code. I've learned to work with
+                HTML, CSS, JavaScript, third-party APIs, Progressive Web Applications, MySQL, API design, and the MERN stack (MongoDB, Express.js, React.js, Node.js) and much more. Allow me to bring your vision to life!</p>
               </div>
             </div>
           </div>
