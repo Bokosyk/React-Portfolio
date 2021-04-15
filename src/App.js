44d7280ea8ff2a69ee34bf49a4d7e0a8115353ec
Leandro Parrado <lparrado321@gmail.com>
2021-04-14 20:30:38 -0400

    Deleted unnecessary components

--- src/App.js
+++ src/App.js
@@ -5,20 +5,20 @@ import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Wrapper from "./components/Wrapper";
+// import Wrapper from "./components/Wrapper";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <Wrapper>
+      
           <Route exact path="/" component={About} />
           <Route exact path="/React-Portfolio" component={About} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
           <Route exact path="/portfolio" component={Portfolio} />
-        </Wrapper>
+       
         <Footer />
       </div>
     </Router>
