3f950d503400dcdb2426c2c24d222d3ed7f277af
Leandro Parrado <lparrado321@gmail.com>
2021-12-10 14:28:17 -0500

    Stylizing Contact box in Info. Also, contact box was moved from about us page to Info component

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -5,11 +5,20 @@ function Info() {
   return (
 
 
-     // PICTURE AND BIO
+    // PICTURE AND BIO
     <section>
       <div>
         <img src="https://raw.githubusercontent.com/Bokosyk/My-Portfolio/main/PICSmall.jpg" alt="Placeholder" className="img-thumbnail" />
       </div>
+
+      {/* BG graphics */}
+      <picture>
+        <figure className="fig1"></figure>
+        <figure className="fig2"></figure>
+        <figure className="fig3"></figure>
+        <figure className="fig4"></figure>
+        <figure className="fig5"></figure>
+      </picture>
 
       <p className="text"> Hey there! I'm Leandro. I was born in Kissimmee, Florida and spent a large
         chunk of my childhood in Las Vegas, Nevada, before moving back down to Florida and have been living here
@@ -18,6 +27,37 @@ function Info() {
       <p className="text">Now graduated in the University of Central Florida's Coding
         Bootcamp, I'm finally pursuing something that is fulfilling and challenges me to grow. A self-motivated, continually learning Full Stack developer with a background in graphic design seeking to contribute to a meaningful open-source project. I enjoy building beautiful applications and figuring out how best to approach a task from a programming and artistic standpoint. I can visualize a product, build it from the ground-up, and make it stand out. I've learned to work with
         HTML, CSS, Javascript, Bootstrap, Jquery, APIs, and much more. Thanks for visiting my web site.</p>
+
+      <aside className="touchbase">
+
+        <h5 className="card-title">Get in Touch</h5>
+
+        
+
+          {/* // <!-- Email input --> */}
+          <div className="input-row col-md-11">
+            <label id="email-label">E-mail</label>
+          </div>
+          <input id="email" className="form-control" type="email" placeholder="Enter your Email" required />
+
+          {/* // <!-- Name input --> */}
+          <div className="input-row col-md-11 ">
+            <label id="name-label">Name</label>
+            <input id="name" className="form-control" type="text" placeholder="Enter your name" required />
+          </div>
+        
+        <div className="input-row col-md-11">
+          <label>Message</label>
+          <textarea rows="" cols="84" className="form-control" name="description" placeholder="Enter your message here..."></textarea>
+        </div>
+
+
+        {/* // <!-- Portfolio images --> */}
+        <div className="col-lg-3">
+          <button type="button" className="btn btn-success">Submit</button>
+        </div>
+
+      </aside>
     </section>
   );
 }
--- src/pages/About.js
+++ src/pages/About.js
@@ -6,46 +6,7 @@ import Footer from '../components/Footer';
 function About() {
   return (
     <article>
-      <Info>
-        <h2> Full Stack Web Developer</h2>
-
-      </Info>
-      <div className="row-align-left">
-
-        <div className="col-md-4">
-          <div className="card mt-2">
-            <div className="card-body">
-
-              <h5 className="card-title">Contact</h5>
-
-              {/* // <!-- Name input --> */}
-              <div className="row first container">
-                <div className="input-row col-md-11 ">
-                  <label id="name-label">Name</label>
-                  <input id="name" className="form-control" type="text" placeholder="Enter your name" required />
-                </div>
-
-                {/* // <!-- Email input --> */}
-                <div className="input-row col-md-11">
-                  <label id="email-label">E-mail</label>
-                </div>
-                <input id="email" className="form-control" type="email" placeholder="Enter your Email" required />
-              </div>
-
-              <div className="input-row col-md-11">
-                <label>Messages</label>
-                <textarea rows="" cols="84" className="form-control" name="description" placeholder="Enter your message here..."></textarea>
-              </div>
-
-
-              {/* // <!-- Portfolio images --> */}
-              <div className="col-lg-3">
-                <button type="button" className="btn btn-success">Submit</button>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Info />
       <Footer />
     </article>
     
