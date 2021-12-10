91273ab09a449ad9b685a1c659d052d97f39a50c
Leandro Parrado <lparrado321@gmail.com>
2021-12-10 15:26:08 -0500

    Centered and stylized info in About

--- src/components/Info/index.js
+++ src/components/Info/index.js
@@ -20,15 +20,17 @@ function Info() {
         <figure className="fig5"></figure>
       </picture>
 
-      <p className="text"> Hey there! I'm Leandro. I was born in Kissimmee, Florida and spent a large
-        chunk of my childhood in Las Vegas, Nevada, before moving back down to Florida and have been living here
-        ever since! In 2017, I graduated from Hialeah Gardens H.S. in Hialeah, FL. I started my journey learning graphic design and decided to dig deeper by learning more about how browsers worked, which eventually led me to web development.</p>
+      <main className="text">
+        <p> Hey there! I'm Leandro. I was born in Kissimmee, Florida and spent a large
+          chunk of my childhood in Las Vegas, Nevada, before moving back down to Florida and have been living here
+          ever since! In 2017, I graduated from Hialeah Gardens H.S. in Hialeah, FL. I started my journey learning graphic design and decided to dig deeper by learning more about how browsers worked, which eventually led me to web development.</p>
 
-      <p className="text">Now graduated in the University of Central Florida's Coding
-        Bootcamp, I'm finally pursuing something that is fulfilling and challenges me to grow. A self-motivated, continually learning Full Stack developer with a background in graphic design seeking to contribute to a meaningful open-source project. I enjoy building beautiful applications and figuring out how best to approach a task from a programming and artistic standpoint. I can visualize a product, build it from the ground-up, and make it stand out. I've learned to work with
-        HTML, CSS, Javascript, Bootstrap, Jquery, APIs, and much more. Thanks for visiting my web site.</p>
+        <p>Now graduated in the University of Central Florida's Coding
+          Bootcamp, I'm finally pursuing something that is fulfilling and challenges me to grow. A self-motivated, continually learning Full Stack developer with a background in graphic design seeking to contribute to a meaningful open-source project. I enjoy building beautiful applications and figuring out how best to approach a task from a programming and artistic standpoint. I can visualize a product, build it from the ground-up, and make it stand out. I've learned to work with
+          HTML, CSS, Javascript, Bootstrap, Jquery, APIs, and much more. Thanks for visiting my web site.</p>
+      </main>
 
-      <aside className="touchbase">
+      <form className="touchbase">
 
         <h5 className="card-title">Get in Touch</h5>
 
@@ -54,7 +56,7 @@ function Info() {
           <a href="/" target="_blank"><button type="button">SUBMIT</button></a>
         </div>
 
-      </aside>
+      </form>
     </section>
   );
 }
