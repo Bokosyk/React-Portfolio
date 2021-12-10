d14e3417a477a59f611ec80ce61815692bca5ed0
Leandro Parrado <lparrado321@gmail.com>
2021-12-10 15:08:46 -0500

    Removed footer information

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,8 +8,6 @@ function Footer() {
       <a href="https://github.com/Bokosyk" className="fa fa-github" target="_blank" rel="noreferrer"> </a>
       <a href="https://www.linkedin.com/in/leandro-parrado-a05729166/" className="fa fa-linkedin" target="_blank" rel="noreferrer"> </a>
       <div className="box">
-        <p><strong>Email:</strong> lparrado321@gmail.com</p>
-        <p><strong>Phone Number</strong> (786) 374-9693</p>
       </div>
 
     </footer>
