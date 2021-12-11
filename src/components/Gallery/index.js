8f1ece00e72151370298b1cbcba76164f689545e
Leandro Parrado <lparrado321@gmail.com>
2021-12-10 21:00:34 -0500

    Stylized portfolio buttons.

--- src/components/Gallery/index.js
+++ src/components/Gallery/index.js
@@ -11,7 +11,7 @@ function Gallery() {
                 <div className="column">
                     <div className="content">
                         <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/Mood.png" alt="Mountains" />
-                        <h3>My Work</h3>
+                        <h3>Mood</h3>
                         <a href="https://github.com/Bokosyk/Mood"><button type="button" className="btn btn-outline-dark">Repository</button></a>
                         <a href="https://bokosyk.github.io/Mood/"><button type="button" className="btn btn-outline-dark">Website</button></a>
                     </div>
@@ -19,7 +19,7 @@ function Gallery() {
                 <div className="column">
                     <div className="content">
                         <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/Planner.png" alt="Lights" />
-                        <h3>My Work</h3>
+                        <h3>Work Day Scheduler</h3>
                         <a href="https://github.com/Bokosyk/PLANNER"><button type="button" className="btn btn-outline-dark">Repository</button></a>
                         <a href="https://bokosyk.github.io/PLANNER/"><button type="button" className="btn btn-outline-dark">Website</button></a>
                     </div>
@@ -27,7 +27,7 @@ function Gallery() {
                 <div className="column">
                     <div className="content">
                         <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/PassGen.png" alt="Nature" />
-                        <h3>My Work</h3>
+                        <h3>Password Generator</h3>
                         <a href="https://github.com/Bokosyk/Password-Generator"><button type="button" className="btn btn-outline-dark">Repository</button></a>
                         <a href="https://bokosyk.github.io/Password-Generator/"><button type="button" className="btn btn-outline-dark">Website</button></a>
                     </div>
@@ -35,7 +35,7 @@ function Gallery() {
                 <div className="column">
                     <div className="content">
                         <img src="https://raw.githubusercontent.com/Bokosyk/Burger/main/public/assets/Images/Burger.png" alt="Mountains" />
-                        <h3>My Work</h3>
+                        <h3>Eat-Da-Burger</h3>
                         <a href="https://github.com/Bokosyk/Burger"><button type="button" className="btn btn-outline-dark">Repository</button></a>
                         <a href="https://stormy-peak-63579.herokuapp.com/"><button type="button" className="btn btn-outline-dark">Website</button></a>
                     </div>
