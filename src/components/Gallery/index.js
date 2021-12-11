6606d3eea7e7cf4ec4f32b8a6ffede1634022a9c
Leandro Parrado <lparrado321@gmail.com>
2021-12-10 20:49:25 -0500

    Just scrapped portfolio content from portfolio.js altogether and moved it into it's own separate component. Much easier to manage. Used a simple css grid for gallery to save myself the trouble of having to stay up late. Might improve on it in the future but for now it works.

---
+++ src/components/Gallery/index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "./style.css";
+
+function Gallery() {
+    return (
+        <main>
+            <h2>PORTFOLIO</h2>
+
+            {/* Portfolio Gallery Grid */}
+            <div className="row">
+                <div className="column">
+                    <div className="content">
+                        <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/Mood.png" alt="Mountains" />
+                        <h3>My Work</h3>
+                        <a href="https://github.com/Bokosyk/Mood"><button type="button" className="btn btn-outline-dark">Repository</button></a>
+                        <a href="https://bokosyk.github.io/Mood/"><button type="button" className="btn btn-outline-dark">Website</button></a>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="content">
+                        <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/Planner.png" alt="Lights" />
+                        <h3>My Work</h3>
+                        <a href="https://github.com/Bokosyk/PLANNER"><button type="button" className="btn btn-outline-dark">Repository</button></a>
+                        <a href="https://bokosyk.github.io/PLANNER/"><button type="button" className="btn btn-outline-dark">Website</button></a>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="content">
+                        <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/PassGen.png" alt="Nature" />
+                        <h3>My Work</h3>
+                        <a href="https://github.com/Bokosyk/Password-Generator"><button type="button" className="btn btn-outline-dark">Repository</button></a>
+                        <a href="https://bokosyk.github.io/Password-Generator/"><button type="button" className="btn btn-outline-dark">Website</button></a>
+                    </div>
+                </div>
+                <div className="column">
+                    <div className="content">
+                        <img src="https://raw.githubusercontent.com/Bokosyk/Burger/main/public/assets/Images/Burger.png" alt="Mountains" />
+                        <h3>My Work</h3>
+                        <a href="https://github.com/Bokosyk/Burger"><button type="button" className="btn btn-outline-dark">Repository</button></a>
+                        <a href="https://stormy-peak-63579.herokuapp.com/"><button type="button" className="btn btn-outline-dark">Website</button></a>
+                    </div>
+                </div>
+            </div>
+        </main >
+    );
+}
+
+export default Gallery;
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,73 +1,12 @@
 import React from "react";
 import Footer from '../components/Footer';
+import Gallery from '../components/Gallery';
 
 
 function Portfolio() {
   return (
     <section>
-
-      <h5>Portfolio</h5>
-
-      {/* <!-- Portfolio image gallery on a CSS grid --> */}
-      <article className="gallery">
-
-        <figure className="gallery__item gallery__item--1">
-          <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/Mood.png" className="gallery__img" alt="Ima 1" />
-          <caption>
-
-          </caption>
-        </figure>
-
-        <figure className="gallery__item gallery__item--2">
-          <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/Planner.png" className="gallery__img" alt="Ima 2" />
-          <caption>
-            
-          </caption>
-        </figure>
-
-        <figure className="gallery__item gallery__item--3">
-          <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/PassGen.png" className="gallery__img" alt="Ima 3" />
-          <caption>
-            
-          </caption>
-        </figure>
-
-        <figure className="gallery__item gallery__item--4">
-          <img src="https://raw.githubusercontent.com/Bokosyk/Burger/main/public/assets/Images/Burger.png" className="gallery__img" alt="Ima 4" />
-          <caption>
-            
-          </caption>
-        </figure>
-
-        {/* <div>
-          <p>Mood</p>
-          <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/Mood.png" alt="Mood Project" className="img-thumbnail" />
-          <a href="https://github.com/Bokosyk/Mood"><button type="button" className="btn btn-outline-dark">Repository</button></a>
-          <a href="https://bokosyk.github.io/Mood/"><button type="button" className="btn btn-outline-dark">Website</button></a>
-        </div>
-        <div>
-          <p>Workday Scheduler</p>
-          <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/Planner.png" alt="Workday Scheduler thumbnail" className="img-thumbnail" />
-          <a href="https://github.com/Bokosyk/PLANNER"><button type="button" className="btn btn-outline-dark">Repository</button></a>
-          <a href="https://bokosyk.github.io/PLANNER/"><button type="button" className="btn btn-outline-dark">Website</button></a>
-        </div>
-        <div>
-          <p>Password Generator</p>
-          <img src="https://raw.githubusercontent.com/Bokosyk/React-Portfolio/main/public/images/PassGen.png" alt="Password Generator thumbnail" className="img-thumbnail" />
-          <a href="https://github.com/Bokosyk/Password-Generator"><button type="button" className="btn btn-outline-dark">Repository</button></a>
-          <a href="https://bokosyk.github.io/Password-Generator/"><button type="button" className="btn btn-outline-dark">Website</button></a>
-        </div>
-
-        <div>
-          <p>Eat Da Burger</p>
-          <img src="https://raw.githubusercontent.com/Bokosyk/Burger/main/public/assets/Images/Burger.png" alt="Password Generator thumbnail" className="img-thumbnail" />
-          <a href="https://github.com/Bokosyk/Burger"><button type="button" className="btn btn-outline-dark">Repository</button></a>
-          <a href="https://stormy-peak-63579.herokuapp.com/"><button type="button" className="btn btn-outline-dark">Website</button></a>
-        </div> */}
-
-
-      </article>
-
+      <Gallery />
       <Footer />
     </section>)
 }
